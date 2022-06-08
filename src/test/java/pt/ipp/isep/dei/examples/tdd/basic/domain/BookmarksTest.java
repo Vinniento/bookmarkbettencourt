3c7e796d956973930df7e2e4d5c4faca5b27542a
Vincent Schauer <schauervincent@gmail.com>
2022-06-08 15:06:10 +0200

    ensureFilteringByKeywordThatDoesNotExistsReturnsZero in Bookmarks

--- src/test/java/pt/ipp/isep/dei/examples/tdd/basic/domain/BookmarksTest.java
+++ src/test/java/pt/ipp/isep/dei/examples/tdd/basic/domain/BookmarksTest.java
@@ -209,17 +209,47 @@ public class BookmarksTest {
         bookmarks.addBookmark(url1);
         bookmarks.addBookmark(url2);
 
-
-        //act
         bookmarks.addTagToBookmark(url, firstTag);
         bookmarks.addTagToBookmark(url, secondTag);
         bookmarks.addTagToBookmark(url2, firstTag);
 
+        //act
 
-        //result
         int result = bookmarks.getBookmarksByTag(firstTag).size();
 
+        //result
+
         int expectedResult = 2;
+
+        assertEquals(expectedResult, result);
+
+
+    }
+
+    @Test
+    public void ensureFilteringByKeywordThatDoesNotExistsReturnsZero() throws MalformedURLException {
+
+        URL url = new URL("http://www.google.com");
+        URL url1 = new URL("http://www.face.com/bla");
+        URL url2 = new URL("http://www.google.at/extra");
+
+        Bookmarks bookmarks = new Bookmarks();
+        //arrange
+        String firstTag = "firstTag";
+        String secondTag = "secondTag";
+        bookmarks.addBookmark(url);
+        bookmarks.addBookmark(url1);
+        bookmarks.addBookmark(url2);
+
+
+        //act
+
+        int result = bookmarks.getBookmarksByTag(firstTag).size();
+
+
+        //result
+
+        int expectedResult = 0;
 
         assertEquals(expectedResult, result);
 
